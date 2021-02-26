680e854c3dd48a226dceba025ebb51f3311501e0
liyq <liyuanquan@qiniu.com>
2021-02-26 18:21:00 +0800

    update

--- ps_parser.go
+++ ps_parser.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"io"
-    "io/ioutil"
-    "bytes"
-    "os"
-    "log"
-    "errors"
+	"io/ioutil"
+	"log"
+	"os"
+
 	"github.com/32bitkid/bitreader"
 )
 
@@ -38,7 +39,7 @@ const (
 )
 
 const (
-	MAXFrameLen        int = 1024 * 1024 * 10
+	MAXFrameLen int = 1024 * 1024 * 10
 )
 
 var (
@@ -49,92 +50,90 @@ var (
 	ErrParseRtp          = errors.New("parse rtp packet error")
 )
 
-/*
-https://github.com/videolan/vlc/blob/master/modules/demux/mpeg
-*/
-type DecPSPackage struct {
-	systemClockReferenceBase      uint64
-	systemClockReferenceExtension uint64
-	programMuxRate                uint32
-
+type PsDecoder struct {
 	rawData         []byte
 	rawLen          int
 	videoStreamType uint32
 	audioStreamType uint32
+	br              bitreader.BitReader
+	psHeader        map[string]uint32
 }
 
-func (dec *DecPSPackage) reSet() {
+func (dec *PsDecoder) reSet() {
 	dec.rawLen = 0
 }
 
-func (dec *DecPSPackage) decPackHeader(br bitreader.BitReader) ([]byte, error) {
+func (dec *PsDecoder) decPs() ([]byte, error) {
 
+	br := dec.br
 	for {
 		startcode, err := br.Read32(32)
 		if err != nil {
-            log.Println(err)
+			log.Println(err)
 			return nil, err
 		}
 
 		switch startcode {
-        case StartCodePS:
-            log.Println("=== ps header ===")
-            if err := dec.decPSHeader(br); err != nil {
-                log.Println(err)
-                return nil, err
-            }
+		case StartCodePS:
+			log.Println("=== ps header ===")
+			if err := dec.decodePsHeader(); err != nil {
+				log.Println(err)
+				return nil, err
+			}
 		case StartCodeSYS:
-            log.Println("=== ps system header === ")
-			if err := dec.decSystemHeader(br); err != nil {
-                log.Println(err)
+			log.Println("=== ps system header === ")
+			if err := dec.decSystemHeader(); err != nil {
+				log.Println(err)
 				return nil, err
 			}
 		case StartCodeMAP:
-            log.Println("=== program stream map ===")
-			if err := dec.decProgramStreamMap(br); err != nil {
+			log.Println("=== program stream map ===")
+			if err := dec.decProgramStreamMap(); err != nil {
 				return nil, err
 			}
 		case StartCodeVideo:
-            log.Println("=== video ===")
-			if err := dec.decPESPacket(br); err != nil {
-                log.Println(err)
+			log.Println("=== video ===")
+			if err := dec.decPESPacket(); err != nil {
+				log.Println(err)
 				return nil, err
 			}
-		case StartCodeAudio: 
-            log.Println("=== audio ===")
+		case StartCodeAudio:
+			log.Println("=== audio ===")
 			raw := dec.rawData[:dec.rawLen]
 			dec.rawLen = 0
 			return raw, nil
 		case MEPGProgramEndCode:
-            log.Println("=== end ===")
+			log.Println("=== end ===")
 			raw := dec.rawData[:dec.rawLen]
 			dec.rawLen = 0
 			return raw, nil
 		default:
-            log.Printf("parse start code error: 0x%x", startcode)
+			log.Printf("parse start code error: 0x%x", startcode)
 			dec.rawLen = 0
 			return nil, ErrParsePakcet
 		}
 	}
 }
 
-func (dec *DecPSPackage) decSystemHeader(br bitreader.BitReader) error {
+func (dec *PsDecoder) decSystemHeader() error {
+	br := dec.br
 	syslens, err := br.Read32(16)
-    log.Printf("\tsystem_header_length:%d", syslens)
+	log.Printf("\tsystem_header_length:%d", syslens)
 	if err != nil {
 		return err
 	}
 
-    br.Skip(uint(syslens) * 8)
+	br.Skip(uint(syslens) * 8)
 	return nil
 }
 
-func (dec *DecPSPackage) decProgramStreamMap(br bitreader.BitReader) error {
+func (dec *PsDecoder) decProgramStreamMap() error {
+	br := dec.br
 	psmLen, err := br.Read32(16)
 	if err != nil {
 		return err
 	}
-    log.Printf("\tprogram_stream_map_length: %d pos: %d", psmLen, br.Size() - int64(br.Len()))
+	//log.Printf("\tprogram_stream_map_length: %d pos: %d", psmLen, br.Size() - int64(br.Len()))
 	//drop psm version infor
 	br.Skip(16)
 	psmLen -= 2
@@ -149,26 +148,26 @@ func (dec *DecPSPackage) decProgramStreamMap(br bitreader.BitReader) error {
 		return err
 	}
 	psmLen -= (2 + programStreamMapLen)
-    log.Printf("\tprogram_stream_info_length: %d", programStreamMapLen)
+	log.Printf("\tprogram_stream_info_length: %d", programStreamMapLen)
 	for programStreamMapLen > 0 {
 		streamType, err := br.Read32(8)
-        log.Printf("\t\tstream type: 0x%x", streamType)
+		log.Printf("\t\tstream type: 0x%x", streamType)
 		if err != nil {
 			return err
 		}
 		elementaryStreamID, err := br.Read32(8)
-        if err != nil {
+		if err != nil {
 			return err
 		} else if elementaryStreamID >= 0xe0 && elementaryStreamID <= 0xef {
 			dec.videoStreamType = streamType
 		} else if elementaryStreamID >= 0xc0 && elementaryStreamID <= 0xdf {
 			dec.audioStreamType = streamType
 		}
-        log.Printf("\t\tstream id: 0x%x", elementaryStreamID)
+		log.Printf("\t\tstream id: 0x%x", elementaryStreamID)
 		if elementaryStreamInfoLength, err := br.Read32(16); err != nil {
 			return err
 		} else {
-            log.Printf("\t\telementary_stream_info_length: %d", elementaryStreamInfoLength)
+			log.Printf("\t\telementary_stream_info_length: %d", elementaryStreamInfoLength)
 			br.Skip(uint(elementaryStreamInfoLength * 8))
 			programStreamMapLen -= (4 + elementaryStreamInfoLength)
 		}
@@ -177,27 +176,27 @@ func (dec *DecPSPackage) decProgramStreamMap(br bitreader.BitReader) error {
 
 	// crc 32
 	if psmLen != 4 {
-        log.Printf("psmLen: 0x%x", psmLen)
+		log.Printf("psmLen: 0x%x", psmLen)
 		return ErrFormatPack
 	}
 	br.Skip(32)
 	return nil
 }
 
-func (dec *DecPSPackage) decPESPacket(br bitreader.BitReader) error {
-
+func (dec *PsDecoder) decPESPacket() error {
+	br := dec.br
 	payloadlen, err := br.Read32(16)
 	if err != nil {
 		return err
 	}
-    log.Printf("\tPES_packet_length: %d", payloadlen)
+	log.Printf("\tPES_packet_length: %d", payloadlen)
 	br.Skip(16)
 
 	payloadlen -= 2
 	if pesHeaderDataLen, err := br.Read32(8); err != nil {
 		return err
 	} else {
-        log.Printf("\tpes_header_data_length: %d", pesHeaderDataLen)
+		log.Printf("\tpes_header_data_length: %d", pesHeaderDataLen)
 		payloadlen--
 		br.Skip(uint(pesHeaderDataLen * 8))
 		payloadlen -= pesHeaderDataLen
@@ -210,105 +209,74 @@ func (dec *DecPSPackage) decPESPacket(br bitreader.BitReader) error {
 		copy(dec.rawData[dec.rawLen:], payloaddata)
 		dec.rawLen += int(payloadlen)
 	}
-    if payloaddata[4] == 0x67 {
-        log.Println("\t\tSPS")
-    }
-    if payloaddata[4] == 0x68 {
-        log.Println("\t\tPPS")
-    }
-    if payloaddata[4] == 0x65 {
-        log.Println("\t\tIDR")
-    }
+	if payloaddata[4] == 0x67 {
+		log.Println("\t\tSPS")
+	}
+	if payloaddata[4] == 0x68 {
+		log.Println("\t\tPPS")
+	}
+	if payloaddata[4] == 0x65 {
+		log.Println("\t\tIDR")
+	}
 
 	return nil
 }
 
-func (dec *DecPSPackage) decPSHeader(br bitreader.BitReader) error {
-	if _, err := br.Read32(2); err != nil {
-		return  err
-	} 
+type FieldInfo struct {
+	len  uint
+	item string
+}
 
-	if s, err := br.Read32(3); err != nil {
-		return  err
-	} else {
-		dec.systemClockReferenceBase |= uint64(s << 30)
-	}
-	if _, err := br.Read32(1); err != nil {
-		return  err
-	} 
+var psheaderFields = []FieldInfo{
+	{2, "fixed"},
+	{3, "system_clock_refrence_base1"},
+	{1, "marker_bit1"},
+	{15, "system_clock_refrence_base2"},
+	{1, "marker_bit2"},
+	{15, "system_clock_refrence_base3"},
+	{1, "marker_bit3"},
+	{9, "system_clock_reference_extension"},
+	{1, "marker_bit4"},
+	{22, "program_mux_rate"},
+	{1, "marker_bit5"},
+	{1, "marker_bit6"},
+	{5, "reserved"},
+	{3, "pack_stuffing_length"},
+}
 
-	if s, err := br.Read32(15); err != nil {
-		return  err
-	} else {
-		dec.systemClockReferenceBase |= uint64(s << 15)
+func (decoder *PsDecoder) decodePsHeader() error {
+	for _, field := range psheaderFields {
+		val, err := decoder.br.Read32(field.len)
+		if err != nil {
+			log.Printf("parse %s error", field.item)
+			return err
+		}
+		decoder.psHeader[field.item] = val
 	}
-	if _, err := br.Read32(1); err != nil {
-		return  err
-	}
-
-	if s, err := br.Read32(15); err != nil {
-		return  err
-	} else {
-		dec.systemClockReferenceBase |= uint64(s)
-	}
-    log.Printf("\tsytem_clock_reference_base: 0x%x", dec.systemClockReferenceBase)
-	if _, err := br.Read32(1); err != nil {
-		return  err
-	}
-	if s, err := br.Read32(9); err != nil {
-		return  err
-	} else {
-		dec.systemClockReferenceExtension |= uint64(s)
-	}
-    log.Printf("\tsystem_clock_reference_extension: 0x%x", dec.systemClockReferenceExtension)
-	if _, err := br.Read32(1); err != nil {
-		return  err
-	} 
-
-	if pmr, err := br.Read32(22); err != nil {
-		return  err
-	} else {
-		dec.programMuxRate |= pmr
-	}
-    log.Printf("\tprogram_mux_rate: %d", dec.programMuxRate)
-	if _, err := br.Read32(1); err != nil {
-		return  err
-	} 
-	if _, err := br.Read32(1); err != nil {
-		return  err
-	} 
-
-	if err := br.Skip(5); err != nil {
-		return  err
-	}
-	if psl, err := br.Read32(3); err != nil {
-		return  err
-	} else {
-        log.Printf("\tpack stuffing length: %d", psl)
-		br.Skip(uint(psl * 8))
-	}
-
-    return nil
-
+	pack_stuffing_length := decoder.psHeader["pack_stuffing_length"]
+	decoder.br.Skip(uint(pack_stuffing_length * 8))
+	return nil
 }
 
 func main() {
-    log.SetFlags(log.LstdFlags | log.Lshortfile)
-    ps_pkt,err := ioutil.ReadFile(os.Args[1])
-    if err != nil {
-        log.Printf("open file: %s error", os.Args[1])
-        return
-    }
+	log.SetFlags(log.Lshortfile)
+	ps_pkt, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Printf("open file: %s error", os.Args[1])
+		return
+	}
 	ps_pkt = append(ps_pkt, 0x00, 0x00, 0x01, 0xb9)
 	br := bitreader.NewReader(bytes.NewReader(ps_pkt))
-    psDec := &DecPSPackage{
-        rawData: make([]byte, MAXFrameLen),
-        rawLen:  0,
-    }
-    psDec.reSet()
-    h264, err := psDec.decPackHeader(br)
-    if err != nil {
-        return
-    }
-    log.Printf("h264 len:%d", len(h264))
+	psDec := &PsDecoder{
+		rawData:  make([]byte, MAXFrameLen),
+		rawLen:   0,
+		br:       br,
+		psHeader: make(map[string]uint32),
+	}
+	psDec.reSet()
+	h264, err := psDec.decPs()
+	if err != nil {
+		return
+	}
+	log.Printf("h264 len:%d", len(h264))
 }
